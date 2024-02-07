293db90e024b22cd44462cdeccc44e0aa6cb5458
Maria Carvalho <miacsilvac@hotmail.com>
2024-02-07 17:58:24 +0000

    playRound function

--- script.js
+++ script.js
@@ -10,4 +10,27 @@ console.log(computerPlay())
 
 function playRound(playerSelection, computerSelection){
 
+    let playerSelectionLowCas = playerSelection.toLowerCase()
+    let computerSelectionLowCas = computerSelection.toLowerCase()
+
+    if (playerSelectionLowCas !== "rock" && playerSelectionLowCas !== "paper" && playerSelectionLowCas !== "scissors") {
+        return "You can only input \"Rock\" \"Paper\" or \"Scissors\" 😐 Try again!";
+    }
+
+    if (playerSelectionLowCas === computerSelectionLowCas) {
+        return "It's a draw";
+      } else if (
+        (playerSelectionLowCas === "rock" && computerSelectionLowCas === "scissors") ||
+        (playerSelectionLowCas === "paper" && computerSelectionLowCas === "rock") ||
+        (playerSelectionLowCas === "scissors" && computerSelectionLowCas === "paper")
+      ) {
+        return `You Win! ${playerSelectionLowCas} beats ${computerSelectionLowCas}`;
+      } else {
+        return `You Lose! ${playerSelectionLowCas} beats ${computerSelectionLowCas}`;
+    }
 }
+
+let playerSelection = "Rock"
+let computerSelection= computerPlay()
+
+console.log(playRound(playerSelection, computerSelection))
