818d9f7a869fca83c92fe7b432f2b115935d8d86
Maria Carvalho <miacsilvac@hotmail.com>
2024-02-07 19:00:12 +0000

    game function parcial

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ function computerPlay () {
     return randomSelection;
 }
 
-console.log(computerPlay())
-
 
 function playRound(playerSelection, computerSelection){
 
@@ -18,7 +16,7 @@ function playRound(playerSelection, computerSelection){
     }
 
     if (playerSelectionLowCas === computerSelectionLowCas) {
-        return "It's a draw";
+        return "It's a draw!";
       } else if (
         (playerSelectionLowCas === "rock" && computerSelectionLowCas === "scissors") ||
         (playerSelectionLowCas === "paper" && computerSelectionLowCas === "rock") ||
@@ -30,7 +28,43 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-let playerSelection = "Rock"
-let computerSelection= computerPlay()
+function game (){
 
-console.log(playRound(playerSelection, computerSelection))
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let round = 1; round <= 5; round++) {
+        let playerSelection = prompt("Round " + round + ": Choose Rock, Paper, or Scissors");
+        let computerSelection = computerPlay();
+
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log("Round " + round + ": " + roundResult);
+
+        if (roundResult.includes("Win")) {
+            playerScore++;
+        } else if (roundResult.includes("Lose")) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You win the game! 🏆");
+    } else if (playerScore < computerScore) {
+        console.log("You lose the game. Try again! 😔");
+    } else {
+        console.log("It's a tie! Try again for a winner. 🤝");
+    }
+}
+
+// game(); 
+
+alert('Hello! You\'ll be playing Rock Paper Scissors with me! And yes I\'m a computer so let the Human ⚔️ Machine Battle start ');
+setTimeout(() => console.log('The game starts in..'), 14000);
+setTimeout(() => console.log('3..'), 15000);
+setTimeout(() => console.log('2..'), 16000);
+setTimeout(() => console.log('1..'), 17000);
+setTimeout(() => game(), 18000);
+
+/* let playerSelection = ""
+let computerSelection= computerPlay();
+console.log(playRound(playerSelection, computerSelection)); */
