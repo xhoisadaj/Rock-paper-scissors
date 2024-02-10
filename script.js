5df2c625254d5d3e768a6114e225f2e953314636
Xhoi Sadaj <xhoisadaj@gmail.com>
2024-02-10 18:36:55 +0100

    modifications

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let result = ['Rock', 'Paper', 'Scissors'];
+let result = ['rock', 'paper', 'scissors'];
 let finishGame = false;
 function computerPlay() {
   let randomSelection = result[Math.floor(Math.random() * result.length)];
@@ -42,47 +42,36 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function cancelGame() {
-  if (confirm('Are you sure you want to quit the game?')) {
-    console.log('%cThank you for playing!', 'color:blue; font-weight:bold;');
-    finishGame = true;
-  }
-}
-
 function playerPlay() {
   let playerSelection = prompt('Type either Rock, Paper, or Scissors!');
 
   if (playerSelection === null) {
-    const choice = prompt(`To quit the game, type "yes" to confirm`)
-      ?.trim()
-      .toLowerCase();
-    if (choice === 'yes') {
+    const choice = confirm('Are you sure you want to quit the game?');
+
+    if (choice) {
       console.log('%cThank you for playing!', 'color:blue; font-weight:bold;');
       finishGame = true;
     } else {
       playerPlay();
     }
   }
+
   if (!finishGame && !isValidInput(playerSelection)) {
     return playerPlay();
   } else {
     return playerSelection;
   }
 }
-
 function playAgain() {
-  let playAgainPrompt = prompt('Want to play again? y/n');
-  if (
-    !playAgainPrompt ||
-    (playAgainPrompt.toLowerCase() !== 'y' &&
-      playAgainPrompt.toLowerCase() !== 'n')
-  ) {
+  let playAgainPrompt = confirm('Do you want to play again?');
+
+  if (!playAgainPrompt) {
+    console.log('%cThank you for playing! 😊', 'color:blue; font-weight:bold;');
     console.log(
-      '%cInvalid option! Options are y(yes) or n(no).',
-      'color:red; font-weight:bold;'
+      '%cMeant to say yes? Type game() in this console',
+      'color:#ccc; font-size: 9px;'
     );
-    playAgain();
-  } else if (playAgainPrompt.toLowerCase() == 'y') {
+  } else {
     console.clear();
     console.log("%cYay! Let's play again 😊", 'color:blue; font-weight:bold;');
     console.log('%cRemember:', 'color:#999; font-weight:bold;');
@@ -94,12 +83,6 @@ function playAgain() {
       'color:#cdbc3a; font-weight:bold;'
     );
     game();
-  } else if (playAgainPrompt.toLowerCase() == 'n') {
-    console.log('%cThank you for playing! 😊', 'color:blue; font-weight:bold;');
-    console.log(
-      '%cMeant to say say? Type game() in this console',
-      'color:#ccc; font-size: 9px;'
-    );
   }
 }
 
